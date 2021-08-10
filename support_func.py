44d5bab1fb25e4b1560678bbab12d3a7faaa195b
nonameuser2019 <52506727+nonameuser2019@users.noreply.github.com>
2021-08-10 13:24:21 +0300

    Update support_func.py

--- support_func.py
+++ support_func.py
@@ -2,37 +2,6 @@ import json
 import pymysql
 from sql_query import *
 
-def count_categories(result):
-    list_id = []
-    for key in result:
-        if key['guid']:
-            list_id.append(key['guid'])
-        if len(key['subcategories']):
-            for sub_key in key['subcategories']:
-                if sub_key ['guid']:
-                    list_id.append(sub_key['guid'])
-                if len(sub_key['subcategories']):
-                    for key in sub_key['subcategories']:
-                        if key['guid']:
-                            list_id.append(key['guid'])
-                        if len(key['subcategories']):
-                            for sub_key in key['subcategories']:
-                                if sub_key['guid']:
-                                    list_id.append(sub_key['guid'])
-                                if len(sub_key['subcategories']):
-                                    for key in sub_key['subcategories']:
-                                        if key['guid']:
-                                            list_id.append(key['guid'])
-                                        if len(key['subcategories']):
-                                            for sub_key in key['subcategories']:
-                                                if sub_key['guid']:
-                                                    list_id.append(sub_key['guid'])
-                                                if len(sub_key['subcategories']):
-                                                    for key in sub_key['subcategories']:
-                                                        if key['guid']:
-                                                            list_id.append(key['guid'])
-    return len(set(list_id))
-
 
 def get_db_conf():
     with open('db.txt', 'r') as f:
